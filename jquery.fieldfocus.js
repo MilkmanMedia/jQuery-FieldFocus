1e384d3ee70111b91bfe50c81ff26295ecb74dc1
MilkmanMedia <daniel.kontos@milkmanmedia.de>
2013-02-27 11:28:02 +0100

    Update jquery.fieldfocus.js

--- jquery.fieldfocus.js
+++ jquery.fieldfocus.js
@@ -3,6 +3,22 @@
  * Description: Small plugin for handling defaulttexts in inputfields
  * Author: Daniel Köntös :: MilkmanMedia - Your WebApp Innovator - www.MilkmanMedia.de
  * License: MIT, GPL
+ * 
+ * Params: 
+ * @defaultText: default value in inputfield (if set to false the value of "value" attribute is used)
+ * @classActive: defines class that is used when field is focussed
+ * @classPassive: defines class that is used when field is not focussed
+ * 
+ * usage:
+ *  
+ * $('input[type="text"]').fieldfocus(); //inits FieldFocus on all textinputs
+ *  
+ * or
+ *
+ * $('input[type="text"], #my-input-field').fieldfocus({
+ *   defaultText: 'hello world!',
+ *   classActive: 'custom-active-class'
+ * }); // inits FieldFocus on all textinputs AND the Element with the ID 'my-input-field'; add the custom default value 'hello world!' and the     custom classActive 'custom-active-class'
  */
 
 (function($){
